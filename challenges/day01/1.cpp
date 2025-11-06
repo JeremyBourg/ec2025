9c0d8580f25e0a30702d51e2f3892a0123b1f97a
Jérémy Bourg <jer.bourg2@gmail.com>
2025-11-06 04:56:32 -0500

    day 1 part 1

---
+++ challenges/day01/1.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+#define ll long long
+
+void split(string &input, string arr[], int& cnt){
+	istringstream stream(input);
+
+	string token;
+
+	while (getline(stream, token, ',')) {
+		arr[cnt++] = token;
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	string nameline, instline;
+	string names[100000];
+	string insts[100000];
+	cin >> nameline;
+	cin >> instline;
+
+	int ncnt=0;
+	int icnt=0;
+
+	split(nameline, names, ncnt);
+	split(instline, insts, icnt);
+
+	int pos=0;
+	for(int i=0; i<icnt; i++) {
+		char dir=insts[i][0];
+		int steps=stoi(insts[i].substr(1));
+
+		if(dir=='R') {
+			pos+=steps;
+
+			if(pos>=ncnt)
+				pos=ncnt-1;
+		}
+		else {
+			pos-=steps;
+
+			if(pos<=0)
+				pos=0;
+		}
+	}
+
+	cout << "Your name is " << names[pos] << endl;
+
+	return 0;
+}
